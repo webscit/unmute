35533beb48b9794ebc8456d8696205531a276cda
Frédéric Collonval <fcollonval@users.noreply.github.com>
2026-03-12 09:37:17 +0100

    Fix web frontend access

--- web/vite.config.ts
+++ web/vite.config.ts
@@ -2,11 +2,10 @@ import path from "path";
 import { defineConfig } from "vitest/config";
 import react from "@vitejs/plugin-react";
 import tailwindcss from "@tailwindcss/vite";
-import basicSsl from "@vitejs/plugin-basic-ssl";
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react(), tailwindcss(), basicSsl()],
+  plugins: [react(), tailwindcss()],
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
